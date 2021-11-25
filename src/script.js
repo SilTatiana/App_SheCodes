d59a08a64c5919adb242f807a05f6c92412aacb2
SilTatiana <siltatiana@icloud.com>
2021-11-25 15:13:35 +0000

    Fixed Units Bug

--- src/script.js
+++ src/script.js
@@ -74,7 +74,7 @@ function loadTemperature(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-  celsiusTemperature = response.data.main.temp;
+ celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
